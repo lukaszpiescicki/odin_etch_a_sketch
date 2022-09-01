39e4fbdb3fdad4a0f884e365ef0b537c509d9f5c
Lukasz <lukasz.piescicki@gmail.com>
2022-09-01 19:25:07 +0200

    Add user defined grid, add hover

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const container = document.querySelector('.container');
-
-
+const button = document.getElementById('btn');
+const quantityVariable = '--quantity';
+let squaresQuantity = 16;
 
 const addSquareToContainer = function(quantity){
-    for(let i = 1; i<=quantity;i++){
+    for(let i = 1; i<=quantity*quantity;i++){
         const square = document.createElement('div');
         square.classList.add(`square`);
         container.appendChild(square);
     }
 }
 
-addSquareToContainer(256);
+const removeSquares = function(){
+    let square = document.querySelector('.square');
+    while(square){
+        container.removeChild(square);
+        square = document.querySelector('.square');
+    }
+}
+
+addSquareToContainer(squaresQuantity);
+
+button.addEventListener('click', function(){
+    
+    let numberOfSquares = prompt('Pass a number of squares');
+    if(numberOfSquares <= 100){
+        removeSquares();
+        squaresQuantity = Number(numberOfSquares)
+        addSquareToContainer(squaresQuantity);    
+        container.style.setProperty(quantityVariable,squaresQuantity);
+    
+    }else{
+        alert('Pass a number higher than 0 and less than 101!');
+    }
+});
